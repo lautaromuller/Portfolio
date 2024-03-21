c8a5b39862c49d910d9b5c5d5895a748eb6c28bd
Lautaro Muller <lautaro8520@gmail.com>
2024-03-21 19:15:00 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const descripciones = document.querySelectorAll('.descripcion')
+const proyectos = document.querySelectorAll('.proyecto')
+
+proyectos.forEach(proyecto => {
+    const idProyecto = proyecto.getAttribute('id')
+    let botonDesc = document.querySelector(`#${idProyecto} .ver-desc`)
+    botonDesc.addEventListener('click', (e => efectoProyecto(e, idProyecto, botonDesc)))
+})
+
+function efectoProyecto(e, id, boton) {
+    let proyectoFocus =  document.querySelector(`#${id} .imagen-proyecto`)
+    let descripcionFocus =  document.querySelector(`#${id} .descripcion`)
+    if (descripcionFocus.hasAttribute('hidden')) {
+        boton.textContent = 'Foto'
+        proyectoFocus.setAttribute('hidden', true)
+        descripcionFocus.removeAttribute('hidden')
+        descripcionFocus.classList.add('estilo-p')
+    }
+    else {
+        boton.textContent = 'Descripción'
+        proyectoFocus.removeAttribute('hidden')
+        descripcionFocus.setAttribute('hidden', true)
+        descripcionFocus.classList.remove('estilo-p')
+    }
+}
+
+///////////////
+
+window.addEventListener('scroll', () => {
+    let nav = document.getElementById('nav')
+    if(window.scrollY > 0) {
+        nav.classList.add('fondoNav')
+    } else {
+        nav.classList.remove('fondoNav')
+    }
+})
+
+//////////////
+
+const links = document.querySelectorAll('.ul-nav a[href^="#"]')
+
+const observador = new IntersectionObserver((entradas) => {
+    entradas.forEach((entrada) => {
+        const id = entrada.target.getAttribute('id')
+        const link = document.querySelector(`.ul-nav a[href="#${id}"]`)
+        
+        if(entrada.isIntersecting){
+            document.querySelector('nav a.colorNav').classList.remove('colorNav')
+            link.classList.add('colorNav');
+        }
+    })
+}, {rootMargin: "-20% 0px -80% 0px"})
+
+links.forEach(link => {
+    const info = link.getAttribute('href')
+    const target = document.querySelector(info)
+    if(target) {
+        observador.observe(target)
+    }
+})
+
+
+///////////////////////
+
+window.addEventListener('load', () => {
+    window.scrollTo(window.scrollX,window.scrollY)
+})
